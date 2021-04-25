47d147eaeee8f3862ec613d0485abe3375239586
Diganta Som <itsdsom111@gmail.com>
2021-04-25 10:59:28 +0530

    Added header

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Header />
+
+      {/* Navbar */}
+
+      {/* Results */}
+    </div>
+  );
+};
+
+export default Home;
