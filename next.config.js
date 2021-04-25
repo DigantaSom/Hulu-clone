3bf218787785dfa1a0585bb5c74b0edb04329d34
Diganta Som <itsdsom111@gmail.com>
2021-04-25 18:54:15 +0530

    Fetch and display results

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['links.papareact.com', 'image.tmdb.com']
+    domains: ['links.papareact.com', 'image.tmdb.org']
   }
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,14 @@ import Head from 'next/head';
 
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
-const Home = () => {
+import requests from '../utils/requests';
+
+// Gets rendered on the client
+const Home = ({ results }) => {
+  console.log(results);
+
   return (
     <div>
       <Head>
@@ -13,10 +19,24 @@ const Home = () => {
 
       <Header />
       <Nav />
-
-      {/* Results */}
+      <Results results={results} />
     </div>
   );
 };
 
 export default Home;
+
+// Gets rendered on the server before client's render
+export const getServerSideProps = async context => {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.trending.url}`
+  ).then(res => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  };
+};
